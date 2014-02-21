dbb7e0d3e2bee30f77ba958f8e6ee2188e7cba5f
rohisingh <rohisingh@deloitte.com>
2014-02-22 01:48:26 +0530

    Dev OnBoarding Test Run Update

--- Blackflag-FVT/Selenium-FVT/src/test/java/ATT/Selenium_FVT/AppOnboarding/MyAppPage/TC_SetupAppValidationPlayground.java
+++ Blackflag-FVT/Selenium-FVT/src/test/java/ATT/Selenium_FVT/AppOnboarding/MyAppPage/TC_SetupAppValidationPlayground.java
@@ -16,50 +16,61 @@ import ATT.Selenium_FVT.Pages.NewAppPage;
 import ATT.Selenium_FVT.Test.TestUtil;
 import ATT.Selenium_FVT.Utilities.Component.Constants;
 
+public class TC_SetupAppValidationPlayground extends TestUtil {
 
-public class TC_SetupAppValidationPlayground extends TestUtil{
+	/**
+	 * Verify that Playground will land on New app creation page when Setup
+	 * Application button is clicked on My apps page
+	 */
 
-	/**  Verify that Playground will land on New app creation page when  Setup Application button is clicked on My apps page */	
-	
 	@Before
-	public void setupPGEnvironment(){
-		
-		APIMLoginPage apimLoginPage= new APIMLoginPage(getNewDriver(Constants.BROWSER));
+	public void setupPGEnvironment() {
+
+		APIMLoginPage apimLoginPage = new APIMLoginPage(
+				getNewDriver(Constants.BROWSER));
 		apimLoginPage.openURL();
-		//method to log into Dev Portal as an OPA
+		// method to log into Dev Portal as an OPA
 		apimLoginPage.playGroundLogin();
-		
-		//method to validate LogIn
+
+		// method to validate LogIn
 		apimLoginPage.validateLogin();
 		MyAppsPage myAppsPage = apimLoginPage.clickMyApps();
 		myAppsPage.deleteSandboxAppPlayground();
-		
+
 		apimLoginPage.publishTestResult();
 		myAppsPage.publishTestResult();
 	}
-	
+
 	@Test
 	public void testSetupAppBtn() {
-		
-/*		APIMLoginPage apimLoginPage= new APIMLoginPage(getNewDriver(Constants.BROWSER));
-		apimLoginPage.openURL();
-		apimLoginPage.pgLogIn();
-		MyAppsPage myAppsPage = apimLoginPage.clickMyApps();*/
+
+		/*
+		 * APIMLoginPage apimLoginPage= new
+		 * APIMLoginPage(getNewDriver(Constants.BROWSER));
+		 * apimLoginPage.openURL(); apimLoginPage.pgLogIn(); MyAppsPage
+		 * myAppsPage = apimLoginPage.clickMyApps();
+		 */
 		MyAppsPage myAppsPage = new MyAppsPage(getDriver());
-		NewAppPage newAppPage =myAppsPage.setUpNewAppAfterCleanup();
-		/** Method to Validate Playground will land on New app creation page on clicking setup new app button*/
+		NewAppPage newAppPage = myAppsPage.setUpNewAppAfterCleanup();
+		/**
+		 * Method to Validate Playground will land on New app creation page on
+		 * clicking setup new app button
+		 */
 		newAppPage.validateNewAppPage();
 	}
+
 	@After
-    public void takeScreenShot() {
-        // take the screenshot at the end of every test
-        File scrFile = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
-        // now save the screenshot to a file some place
-        try {
-			FileUtils.copyFile(scrFile, new File("c:\\tmp\\TC_SetupAppValidationPlayground.png"));
+	public void takeScreenShot() {
+		// take the screenshot at the end of every test
+		File scrFile = ((TakesScreenshot) getDriver())
+				.getScreenshotAs(OutputType.FILE);
+		// now save the screenshot to a file some place
+		try {
+			FileUtils.copyFile(scrFile, new File(
+					"c:\\tmp\\TC_SetupAppValidationPlayground.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+	}
 }
